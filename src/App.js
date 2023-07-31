de1901f7663dcd9a5aba0da122972a7d99cb2fa3
Erkut Gürkan <111708975+sirrsolaire@users.noreply.github.com>
2023-07-31 21:36:28 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,70 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurr, setFromCurr] = useState("USD");
+  const [toCurr, setToCurr] = useState("TRY");
+  const [converted, setConverted] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(
+    function () {
+      async function convert() {
+        if (amount <= 0) return;
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurr}&to=${toCurr}`
+        );
+        const data = await res.json();
+        setConverted(data.rates[toCurr]);
+        setIsLoading(false);
+      }
+
+      if (fromCurr === toCurr) return setConverted(amount);
+      convert();
+    },
+    [amount, fromCurr, toCurr]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <input
+        type="text"
+        value={amount}
+        onChange={(e) => setAmount(Number(e.target.value))}
+        placeholder="Enter value"
+        disabled={isLoading}
+        style={{ marginRight: 10 }}
+      />
+      <select
+        value={fromCurr}
+        onChange={(e) => setFromCurr(e.target.value)}
+        disabled={isLoading}
+        style={{ marginRight: 10 }}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="TRY">TL</option>
+      </select>
+      <span>to</span>
+      <select
+        value={toCurr}
+        onChange={(e) => setToCurr(e.target.value)}
+        disabled={isLoading}
+        style={{ marginLeft: 10 }}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="TRY">TL</option>
+      </select>
+
+      <p>
+        {converted}
+        <span style={{ marginLeft: 8 }}>{toCurr}</span>
+      </p>
     </div>
   );
 }
-
-export default App;
